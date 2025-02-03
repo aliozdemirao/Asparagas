9bee93eb28c7bd2bcdfc4fdd99f8e4546c7cda81
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-02-03 21:19:21 +0300

    Implement news list in HomeFragment
    The changes include:
    - Added `HomeViewModel` and `HomeAdapter` for managing and displaying news data in `HomeFragment`.
    - Set up a RecyclerView in `HomeFragment` to display news articles.
    - Implemented `observeViewModel()` to observe and process data from `HomeViewModel`.
    - Handled `Resource.Success` and `Resource.Error` states of data from `HomeViewModel`.
    - Configured the RecyclerView layout as a vertical LinearLayoutManager.
    - Set up `setupRecyclerView` function to bind news list.
    - Updated `onCreateView` and `onViewCreated` to set up the RecyclerView and observe the ViewModel.

--- app/src/main/java/com/aliozdemir/asparagas/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/aliozdemir/asparagas/presentation/home/HomeFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aliozdemir.asparagas.databinding.FragmentHomeBinding
+import com.aliozdemir.asparagas.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +18,10 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    private lateinit var homeAdapter: HomeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +31,40 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.news.observe(viewLifecycleOwner) { resource ->
+            when (resource) {
+                is Resource.Success -> {
+                    resource.data.articles?.let { articles ->
+                        homeAdapter = HomeAdapter(articles)
+                        binding.rvNewsList.adapter = homeAdapter
+                    }
+                }
+
+                is Resource.Error -> {
+                    resource.message.let { error ->
+                        Toast.makeText(context, resource.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerView() {
+        homeAdapter = HomeAdapter(emptyList())
+        binding.rvNewsList.apply {
+            adapter = homeAdapter
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        }
     }
 
     override fun onDestroyView() {
