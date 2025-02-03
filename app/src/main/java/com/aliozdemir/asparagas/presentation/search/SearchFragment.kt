37c8d42e3b3a8ac2c399e155d9f34bea193a478b
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-02-03 15:27:34 +0300

    Integrate Android Navigation Component
    The Android Navigation Component has been integrated into the project to manage in-app navigation.
    The changes include:
    - Added `nav_graph.xml` which contains the navigation graph.
    - Added `HomeFragment`, `SearchFragment`, `BookmarkFragment`, `DetailFragment` classes.
    - Added layouts `fragment_home.xml`, `fragment_search.xml`, `fragment_bookmark.xml`, `fragment_detail.xml`
    - Added actions to navigate from `HomeFragment`, `SearchFragment` and `BookmarkFragment` to `DetailFragment`.

---
+++ app/src/main/java/com/aliozdemir/asparagas/presentation/search/SearchFragment.kt
@@ -0,0 +1,31 @@
+package com.aliozdemir.asparagas.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aliozdemir.asparagas.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
