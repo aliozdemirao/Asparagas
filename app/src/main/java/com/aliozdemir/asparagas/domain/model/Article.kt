6c145ea480bb7cdeef92e385d5828ada80624501
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-02-03 22:14:08 +0300

    Implement Parcelable for Article and Source data classes and add navigation argument
    The `Article` and `Source` data classes have been updated to implement the `Parcelable` interface using the `@Parcelize` annotation.
    The navigation graph was updated to add an `article` argument of type `Article` to the `detailFragment` destination. This enables passing `Article` objects between fragments via navigation.

--- app/src/main/java/com/aliozdemir/asparagas/domain/model/Article.kt
+++ app/src/main/java/com/aliozdemir/asparagas/domain/model/Article.kt
@@ -1,5 +1,9 @@
 package com.aliozdemir.asparagas.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Article(
     val source: Source?,
     val author: String?,
@@ -9,4 +13,4 @@ data class Article(
     val urlToImage: String?,
     val publishedAt: String?,
     val content: String?,
-)
+) : Parcelable
