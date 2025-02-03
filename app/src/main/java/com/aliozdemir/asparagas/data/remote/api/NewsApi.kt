d6242e5d7168b7181779b51dd6296db31fd01426
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-02-03 16:46:03 +0300

    Add `getTopHeadlines` function to `NewsApi`
    The `getTopHeadlines` function has been added to the `NewsApi` interface. This function retrieves top headlines from the news API and accepts optional query parameters such as `country`, `category`, `sources`, `query`, `pageSize`, and `page`.

---
+++ app/src/main/java/com/aliozdemir/asparagas/data/remote/api/NewsApi.kt
@@ -0,0 +1,17 @@
+package com.aliozdemir.asparagas.data.remote.api
+
+import com.aliozdemir.asparagas.data.remote.dto.NewsDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("sources") sources: String?,
+        @Query("q") query: String?,
+        @Query("pageSize") pageSize: Int?,
+        @Query("page") page: Int?,
+    ): NewsDto
+}
