e3a6aed0f09d8791528c58318f5312eb9430f06b
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-02-03 20:54:46 +0300

    Create `HomeViewHolder` class for RecyclerView
    The `HomeViewHolder` class has been created to manage the `ItemHomeNewsBinding` layout for the RecyclerView.
    It includes the `bind` method to set the values of title, description, published date and image from the `Article` model to the `ItemHomeNewsBinding`.

---
+++ app/src/main/java/com/aliozdemir/asparagas/presentation/home/HomeViewHolder.kt
@@ -0,0 +1,17 @@
+package com.aliozdemir.asparagas.presentation.home
+
+import androidx.recyclerview.widget.RecyclerView
+import com.aliozdemir.asparagas.databinding.ItemHomeNewsBinding
+import com.aliozdemir.asparagas.domain.model.Article
+import com.aliozdemir.asparagas.util.loadImage
+
+class HomeViewHolder(private val binding: ItemHomeNewsBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(article: Article) {
+        with(binding) {
+            tvNewsTitle.text = article.title
+            tvNewsDescription.text = article.description
+            tvNewsPublishedDate.text = article.publishedAt
+            ivNewsImage.loadImage(article.urlToImage)
+        }
+    }
+}
